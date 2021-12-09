c63ff689eccee09428d4e7502feafde764c7fae3
FM <falk@smartlane.de>
2021-12-09 09:27:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "@mui/material";
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
